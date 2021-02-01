dc10746ee4423b4a4407e04852172d84829648fa
Christiaan van Zyl <christiaanvanzyl@gmail.com>
2021-02-01 16:44:33 +0100

    webcomponent

--- webcomponent/static/animals.vue
+++ webcomponent/static/animals.vue
@@ -1,4 +1,5 @@
 <script>
 // See https://vuejs.org/v2/guide/single-file-components.html
-// The custom element <ts-animals> must a self container VueJS app
+// and https://itnext.io/vuidget-how-to-create-an-embeddable-vue-js-widget-with-vue-custom-element-674bdcb96b97
+// The custom element <ts-animals> must be a self contained VueJS app
 </script>
