29916b81590707eddf1ba89feeb5437115e27d48
Christiaan van Zyl <christiaanvanzyl@gmail.com>
2022-09-03 17:12:55 +0200

    Fetch API example

--- src/http/examples.ts
+++ src/http/examples.ts
@@ -1,5 +1,25 @@
 export class Examples {
     async run() {
         console.log("running...")
+        // TODO See ts/http/app.ts
+
+        // Example as per https://blog.logrocket.com/axios-vs-fetch-best-http-requests/
+        const url = "https://jsonplaceholder.typicode.com/todos";
+        const options = {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json;charset=UTF-8",
+            },
+            body: JSON.stringify({
+                a: 10,
+                b: 20,
+            }),
+        };
+        fetch(url, options)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+            });
     }
 }
