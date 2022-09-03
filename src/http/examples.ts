320a391473e4f62d433735b5f064f2b5d086dd3b
Christiaan van Zyl <christiaanvanzyl@gmail.com>
2022-09-03 18:13:14 +0200

    Interface definitions for response types

--- src/http/examples.ts
+++ src/http/examples.ts
@@ -1,7 +1,20 @@
+import { sprintf } from "sprintf-js";
+import { ResponsePOST } from "./httpbin";
+
+// Examples inspired by 
+// https://blog.logrocket.com/axios-vs-fetch-best-http-requests/
 export namespace examples {
+    let baseURL = "https://httpbin.org"
+
+    // TODO Use local containerized services?
+    // setBaseURL is useful for using a locally hosted httpbin
+    export function setBaseURL(s: string) {
+        baseURL = s
+    }
+
     export function post() {
-        // Example as per https://blog.logrocket.com/axios-vs-fetch-best-http-requests/
-        const url = "https://jsonplaceholder.typicode.com/todos";
+        console.info("post")
+        const url = sprintf("%s/post", baseURL);
         const options = {
             method: "POST",
             headers: {
@@ -15,8 +28,38 @@ export namespace examples {
         };
         fetch(url, options)
             .then((response: Response) => response.json())
-            .then((data) => {
-                console.log(data);
+            .then((data: ResponsePOST) => {
+                console.info(data);
             });
     }
+
+    // TODO
+    export function timeout() {
+        const controller = new AbortController();
+        const options = {
+            method: 'POST',
+            signal: controller.signal,
+            body: JSON.stringify({
+                firstName: 'David',
+                lastName: 'Pollock'
+            })
+        };
+        const promise = fetch('/login', options);
+        // @ts-ignore
+        const timeoutId = setTimeout(() => controller.abort(), 4000);
+
+        promise
+            .then(response => { console.info(response) })
+            .catch(error => console.error('timeout exceeded', error));
+    }
+
+    export function interceptor() {
+    }
+
+    export function progress() {
+    }
+
+    export function simultaneous() {
+    }
+
 }
