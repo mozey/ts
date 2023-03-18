c9b3f3492b9e9f3e0cb04750c2aaca20b11461e1
Christiaan B van Zyl <christiaanvanzyl@gmail.com>
2023-03-18 18:16:47 +0100

    Examples

--- src/http-fetch-wrapper/fetch.ts
+++ src/http-fetch-wrapper/fetch.ts
@@ -1,24 +1,38 @@
 import { sprintf } from "sprintf-js"
 import { HttpError } from "./error"
+import { HttpbinResp } from "../http/models/httpbin";
 
 // Fetch Wrapper in TypeScript
 // https://eckertalex.dev/blog/typescript-fetch-wrapper
 async function http<T>(path: string, config: RequestInit): Promise<T> {
   const request = new Request(path, config)
   const response = await fetch(request)
+  
+  let method: string | null = null
+  if (config.method) {
+    method = config.method
+  }
 
   if (!response.ok) {
-    let method = null
-    if (config.method) {
-      method = config.method
-    }
     throw new HttpError(method, path,
         sprintf("%i %s", response.status, response.statusText))
   }
 
-  return response.json().catch(
-    // If there is no body return empty object
-    () => ({}))
+  // Can't do this with httpbin response
+  // return response.json().catch(
+  //   // If there is no body return empty object
+  //   () => ({}))
+
+  // Filter httpbin response, contact is echoed back in the "json" property
+  // https://httpbin.org/#/Anything/post_anything
+  return new Promise((resolve) => {
+    response.json().then((httpbinResp: HttpbinResp) => {
+      resolve(httpbinResp.json)
+    }).catch(() => {
+      throw new HttpError(method, path,
+        sprintf("%i %s", response.status, response.statusText))
+    })
+  })
 }
 
 export async function get<T>(
--- src/http-fetch-wrapper/index.ts
+++ src/http-fetch-wrapper/index.ts
@@ -2,9 +2,50 @@ import { client } from "./client";
 
 export namespace index {
   export function read() {
-    // TODO Show response in #results-template and error handling
     client.contacts.read("foo").then((contact) => {
-      console.info("contact", contact)
+      // Note the type on the variable that is passed in
+      console.info("contact name", contact.name)
+      setHttpbinResp("Read contact", contact)
     })
+  }
+
+  export function create() {
+    client.contacts.add({name: "foo"}).then((contact) => {
+      setHttpbinResp("Create contact", contact)
+    })
+  }
+
+  export function update() {
+    client.contacts.update({name: "foo"}).then((contact) => {
+      setHttpbinResp("Update contact", contact)
+    })
+  }
+
+  export function list() {
+    client.contacts.list().then((contact) => {
+      setHttpbinResp("List contacts", contact)
+    })
+  }
+
+  function setHttpbinResp(example: string, resp: any) {
+    let respJSON = JSON.stringify(resp, null, 4)
+
+    // Clone
+    let panel =
+      document.getElementById("results-template") as HTMLTemplateElement
+    let clone = panel.content.cloneNode(true) as HTMLDivElement
+
+    // Append template
+    let results = document.getElementById("results") as HTMLDivElement
+    results.textContent = ""
+    results.appendChild(clone)
+
+    // Set template values...
+    // Header
+    let header = document.getElementById("header") as HTMLHeadElement
+    header.innerText = example
+    // Resp
+    let respElem = document.getElementById("resp") as HTMLPreElement
+    respElem.innerText = respJSON
   }
 }
